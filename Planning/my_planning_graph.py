9fd21d7401e790006d82c67daae894badf98bda9
Hassan Latif <hassan.latif@gmail.com>
2017-04-15 15:05:43 +0300

    finished

--- Planning/my_planning_graph.py
+++ Planning/my_planning_graph.py
@@ -2,6 +2,7 @@ from aimacode.planning import Action
 from aimacode.search import Problem
 from aimacode.utils import expr
 from lp_utils import decode_state
+from aimacode.logic import PropKB
 
 
 class PgNode():
@@ -315,15 +316,21 @@ class PlanningGraph():
         #   set iff all prerequisite literals for the action hold in S0.  This can be accomplished by testing
         #   to see if a proposed PgNode_a has prenodes that are a subset of the previous S level.  Once an
         #   action node is added, it MUST be connected to the S node instances in the appropriate s_level set.
+
         self.a_levels.append(set())
+
         for action in self.all_actions:
             pgNode_action = PgNode_a(action)
-            for pre_s_node in pgNode_action.prenodes:
-                for s_node in self.s_levels[level]:
-                    if pre_s_node == s_node:
-                       pgNode_action.parents.add(pre_s_node)
-                       s_node.children.add(pgNode_action)
-                       self.a_levels[level].add(pgNode_action)
+            if pgNode_action.prenodes.issubset(self.s_levels[level]):                   
+                self.a_levels[level].add(pgNode_action)                       
+
+        #Setting children/parent 
+        for state in self.s_levels[level]:
+            for action in self.a_levels[level]:
+                if state in action.prenodes:
+                    state.children.add(action)
+                    action.parents.add(state)
+
 
 
     def add_literal_level(self, level):
@@ -343,13 +350,15 @@ class PlanningGraph():
         #   may be "added" to the set without fear of duplication.  However, it is important to then correctly create and connect
         #   all of the new S nodes as children of all the A nodes that could produce them, and likewise add the A nodes to the
         #   parent sets of the S nodes
+
         self.s_levels.append(set())
         prev_actions = self.a_levels[level-1]
         for prev_action in prev_actions:
             for eff_s_node in prev_action.effnodes:
-                prev_action.children.add(eff_s_node)
-                eff_s_node.parents.add(prev_action)
-                self.s_levels[level].add(eff_s_node)
+                self.s_levels[level].add(eff_s_node)        
+                if eff_s_node in prev_action.effect_s_nodes():
+                   prev_action.children.add(eff_s_node)
+                   eff_s_node.parents.add(prev_action)
 
 
     def update_a_mutex(self, nodeset):
